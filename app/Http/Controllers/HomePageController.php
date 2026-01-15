52c63f6a3290518a46b88c43ff822e0d3177c295
nowshadsdc-byte <nowshad.sdc@gmail.com>
2026-01-15 15:33:59 +0600

    add update

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
+use Inertia\Inertia;
 class HomePageController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('welcome');
+    }
 }
