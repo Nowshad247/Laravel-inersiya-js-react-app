1c2eb2568889543246c8136835603baf446c5619
Nowshad <lewsedmond@gmail.com>
2026-01-21 01:40:37 +0600

    btn add lead

--- resources/js/pages/lead/index.tsx
+++ resources/js/pages/lead/index.tsx
@@ -19,8 +19,12 @@ export default function Index({ data }: { data: any }) {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Lead Dashboard" />
-            <div>
-                <Button onClick={() => router.get('/leads/upload')} className='w-3/12 my-6 mx-6'>Bulk Upload</Button>
+            <div className='w-[100%] flex items-center justify-between'>
+                <Button onClick={() => router.get('/leads/upload')} className='w-3/12 my-2 mx-2'>Bulk Upload</Button>
+                <Button onClick={() => router.get('/leads/upload')} className='w-3/12 my-2 mx-2'>Add lead </Button>
+                <Button onClick={() => router.get('/leads/upload')} className='w-3/12 my-2 mx-2'>Today Task</Button>
+                <Button onClick={() => router.get('/leads/upload')} className='w-3/12 my-2 mx-2'>Call Now</Button>
+
             </div>
             <div className='m-6'>
                 <h1 className='text-2xl font-bold'>Lead Dashboard</h1>
